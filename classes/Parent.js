45b591ccfb681549e0641fe8967a326154fa8ad1
TumoArmanElm <arman.elmajyan.y@tumo.org>
2022-11-20 18:45:08 +0400

    Season Feature

--- classes/Parent.js
+++ classes/Parent.js
@@ -3,6 +3,7 @@ class Parent {
     this.x = x;
     this.y = y;
     this.id = id;
+    this.season = "Summer";
     this.matrix = matrix;
     this.objectsMatrix = objectsMatrix;
     this.energy = 0;
@@ -22,86 +23,93 @@ class Parent {
     this.updateCoordinates();
     let found = [];
     for (let i = 0; i < this.directions.length; i++) {
-        let x = this.directions[i][0];
-        let y = this.directions[i][1];
-        if (x >= 0 && x < this.matrix[0].length && y >= 0 && y < this.matrix.length) {
-            if (this.matrix[y][x] == characterId) {
-                found.push(this.directions[i]);
-            }
+      let x = this.directions[i][0];
+      let y = this.directions[i][1];
+      if (
+        x >= 0 &&
+        x < this.matrix[0].length &&
+        y >= 0 &&
+        y < this.matrix.length
+      ) {
+        if (this.matrix[y][x] == characterId) {
+          found.push(this.directions[i]);
         }
+      }
     }
 
     return found;
-}
+  }
 
-updateCoordinates() {
+  updateCoordinates() {
     this.directions = [
-        [this.x - 1, this.y - 1],
-        [this.x, this.y - 1],
-        [this.x + 1, this.y - 1],
-        [this.x - 1, this.y],
-        [this.x + 1, this.y],
-        [this.x - 1, this.y + 1],
-        [this.x, this.y + 1],
-        [this.x + 1, this.y + 1]
+      [this.x - 1, this.y - 1],
+      [this.x, this.y - 1],
+      [this.x + 1, this.y - 1],
+      [this.x - 1, this.y],
+      [this.x + 1, this.y],
+      [this.x - 1, this.y + 1],
+      [this.x, this.y + 1],
+      [this.x + 1, this.y + 1],
     ];
-}
+  }
 
-move(moveEnergyLose) {
+  move(moveEnergyLose) {
     let targetCells = this.chooseCell(0);
     let newCell = random(targetCells);
 
     if (this.energy > 0 && newCell) {
-        let newX = newCell[0];
-        let newY = newCell[1];
+      let newX = newCell[0];
+      let newY = newCell[1];
 
-        this.matrix[newY][newX] = this.id;
-        this.matrix[this.y][this.x] = 0;
+      this.matrix[newY][newX] = this.id;
+      this.matrix[this.y][this.x] = 0;
 
-        this.objectsMatrix[newY][newX] = this;
-        this.objectsMatrix[this.y][this.x] = null;
+      this.objectsMatrix[newY][newX] = this;
+      this.objectsMatrix[this.y][this.x] = null;
 
-        this.x = newX;
-        this.y = newY;
+      this.x = newX;
+      this.y = newY;
 
-        this.energy -= moveEnergyLose;
+      if (this.season == "Winter" || this.season == "Summer") {
+        this.energy -= moveEnergyLose + 0.5;
+        console.log(this.season);
+      } else this.energy -= moveEnergyLose;
     }
 
     this.die();
+  }
 
-}
-
-eat(eatTargetId, energyGain, moveEnergyLose) {
+  eat(eatTargetId, energyGain, moveEnergyLose) {
     let targetCells = this.chooseCell(eatTargetId);
     let newCell = random(targetCells);
 
     if (this.energy > 0 && newCell) {
-        let newX = newCell[0];
-        let newY = newCell[1];
+      let newX = newCell[0];
+      let newY = newCell[1];
 
-        this.matrix[newY][newX] = this.id;
-        this.matrix[this.y][this.x] = 0;
+      this.matrix[newY][newX] = this.id;
+      this.matrix[this.y][this.x] = 0;
 
-        this.objectsMatrix[newY][newX] = this;
-        this.objectsMatrix[this.y][this.x] = null;
+      this.objectsMatrix[newY][newX] = this;
+      this.objectsMatrix[this.y][this.x] = null;
 
-        this.x = newX;
-        this.y = newY;
+      this.x = newX;
+      this.y = newY;
 
+      if (this.season == "Winter" || this.season == "Summer") {
         this.energy += energyGain;
+      } else {this.energy += energyGain + 0.5; console.log("MORE ENERGYYYY");}
+      
 
-        this.multiply();
+      this.multiply();
     } else {
-        this.move(moveEnergyLose);
+      this.move(moveEnergyLose);
     }
-}
-die() {
-
+  }
+  die() {
     if (this.energy <= 0) {
-        this.matrix[this.y][this.x] = 0;
-        this.objectsMatrix[this.y][this.x] = null;
+      this.matrix[this.y][this.x] = 0;
+      this.objectsMatrix[this.y][this.x] = null;
     }
-
-}
-
+  }
 }
--- script.js
+++ script.js
@@ -12,118 +12,127 @@ const seasons = ["Summer", "Fall", "Winter", "Spring"];
 let weather = seasons[0];
 
 function setup() {
-    createCanvas(horizontallength * side, verticallength * side);
-    background('#acacac');
-    frameRate(5);
+  createCanvas(horizontallength * side, verticallength * side);
+  background("#acacac");
+  frameRate(4);
 }
 function draw() {
-    drawMatrix(matrix);
-    updateObjectsMatrix(objectsMatrix);
-    weatherChanger();
+  drawMatrix(matrix);
+  updateObjectsMatrix(objectsMatrix, weather);
+  weatherChanger();
 }
 
-
 function createObjectsMatrix(matrix) {
-    const newObjectsMatrix = [];
-    for (let y = 0; y < matrix.length; y++) {
-        newObjectsMatrix[y] = [];
-        for (let x = 0; x < matrix[y].length; x++) {
-            if (matrix[y][x] == 1) {
-                const newGrass = new Grass(x, y, 1, matrix, newObjectsMatrix);
-                newObjectsMatrix[y][x] = newGrass;
-            } else if (matrix[y][x] == 2) {
-                const newGrassEater = new GrassEater(x, y, 2, matrix, newObjectsMatrix);
-                newObjectsMatrix[y][x] = newGrassEater;
-            } else if (matrix[y][x] == 3) {
-                const newPredator = new Predator(x, y, 3, matrix, newObjectsMatrix);
-                newObjectsMatrix[y][x] = newPredator;
-            } else if (matrix[y][x] == 4) {
-                const newFarmer = new Farmer(x, y, 4, matrix, newObjectsMatrix)
-                newObjectsMatrix[y][x] = newFarmer;
-            } else if (matrix[y][x] == 5) {
-                const newZombie = new Zombie(x, y, 5, matrix, newObjectsMatrix)
-                newObjectsMatrix[y][x] = newZombie;
-            } else {
-                newObjectsMatrix[y][x] = null;
-            }
-        }
+  const newObjectsMatrix = [];
+  for (let y = 0; y < matrix.length; y++) {
+    newObjectsMatrix[y] = [];
+    for (let x = 0; x < matrix[y].length; x++) {
+      if (matrix[y][x] == 1) {
+        const newGrass = new Grass(x, y, 1, matrix, newObjectsMatrix);
+        newObjectsMatrix[y][x] = newGrass;
+      } else if (matrix[y][x] == 2) {
+        const newGrassEater = new GrassEater(x, y, 2, matrix, newObjectsMatrix);
+        newObjectsMatrix[y][x] = newGrassEater;
+      } else if (matrix[y][x] == 3) {
+        const newPredator = new Predator(x, y, 3, matrix, newObjectsMatrix);
+        newObjectsMatrix[y][x] = newPredator;
+      } else if (matrix[y][x] == 4) {
+        const newFarmer = new Farmer(x, y, 4, matrix, newObjectsMatrix);
+        newObjectsMatrix[y][x] = newFarmer;
+      } else if (matrix[y][x] == 5) {
+        const newZombie = new Zombie(x, y, 5, matrix, newObjectsMatrix);
+        newObjectsMatrix[y][x] = newZombie;
+      } else {
+        newObjectsMatrix[y][x] = null;
+      }
     }
-    return newObjectsMatrix;
+  }
+  return newObjectsMatrix;
 }
 
 function drawMatrix(matrix) {
-    for (var y = 0; y < matrix.length; y++) {
-        for (var x = 0; x < matrix[y].length; x++) {
-            if (matrix[y][x] == 1) {
-                fill('green');
-            } else if (matrix[y][x] == 2) {
-                fill('yellow');
-            } else if (matrix[y][x] == 3) {
-                fill('red');
-            } else if (matrix[y][x] == 4) {
-                fill('blue');
-            } else if (matrix[y][x] == 5) {
-                fill('purple');
-            } else {
-                fill('grey');
-            }
-            rect(x * side, y * side, side, side)
-        }
+  for (var y = 0; y < matrix.length; y++) {
+    for (var x = 0; x < matrix[y].length; x++) {
+      if (matrix[y][x] == 1) {
+        seasonColor('green', '#875b09', '#d9e6cf', '#042b0f')
+      } else if (matrix[y][x] == 2) {
+        fill("yellow");
+      } else if (matrix[y][x] == 3) {
+        fill("red");
+      } else if (matrix[y][x] == 4) {
+        fill("blue");
+      } else if (matrix[y][x] == 5) {
+        fill("purple");
+      } else {
+        fill("grey");
+      }
+      rect(x * side, y * side, side, side);
     }
+  }
 }
-function updateObjectsMatrix(objectsMatrix) {
-    for (var y = 0; y < objectsMatrix.length; y++) {
-        for (let x = 0; x < objectsMatrix[y].length; x++) {
-            const object = objectsMatrix[y][x];
-            if (object) {
-                object.update();
-            }
-        }
+function updateObjectsMatrix(objectsMatrix,season) {
+  for (var y = 0; y < objectsMatrix.length; y++) {
+    for (let x = 0; x < objectsMatrix[y].length; x++) {
+      const object = objectsMatrix[y][x];
+      if (object) {
+        object.update();
+        object.season = season;
+      }
     }
+  }
 }
 
 function createMatrix(horizontallength, verticallength) {
-    const newMatrix = [];
-    for (var y = 0; y < verticallength; y++) {
-        newMatrix[y] = [];
-        for (var x = 0; x < horizontallength; x++) {
-            const coursor = Math.random() * 100;
-            if (coursor <= 65) {
-                newMatrix[y][x] = 1;
-            } else if (coursor <= 66.3) {
-                newMatrix[y][x] = 2;
-            } else if (coursor < 68) {
-                newMatrix[y][x] = 3;
-            } else if (coursor < 69.6) {
-                newMatrix[y][x] = 4;
-            } else if (coursor < 70) {
-                newMatrix[y][x] = 5;
-            } else {
-                newMatrix[y][x] = 0;
-            }
-        }
+  const newMatrix = [];
+  for (var y = 0; y < verticallength; y++) {
+    newMatrix[y] = [];
+    for (var x = 0; x < horizontallength; x++) {
+      const coursor = Math.random() * 100;
+      if (coursor <= 65) {
+        newMatrix[y][x] = 1;
+      } else if (coursor <= 66.3) {
+        newMatrix[y][x] = 2;
+      } else if (coursor < 68) {
+        newMatrix[y][x] = 3;
+      } else if (coursor < 69.6) {
+        newMatrix[y][x] = 4;
+      } else if (coursor < 70) {
+        newMatrix[y][x] = 5;
+      } else {
+        newMatrix[y][x] = 0;
+      }
     }
-    return newMatrix;
-};
+  }
+  return newMatrix;
+}
 
-let weatherTimer = 10;
+let weatherTimer = 15;
 
 function weatherChanger() {
+  while (0 < weatherTimer) {
+    weatherTimer--;
+    return;
+  }
+  if (seasonsId >= seasons.length - 1) {
+    seasonsId = 0;
+    weather = seasons[seasonsId];
+    console.log(weather);
+  } else {
+    seasonsId++;
+    weather = seasons[seasonsId];
+    console.log(weather);
+  }
+  weatherTimer = 15;
+}
 
-    while (0 < weatherTimer) {
-        weatherTimer--;
-        return;
-    }
-    if (seasonsId >= seasons.length - 1) {
-        seasonsId = 0;
-        weather = seasons[seasonsId];
-        console.log(weather);
-
-    } else {
-        seasonsId++;
-        weather = seasons[seasonsId];
-        console.log(weather);
-
-    }
-    weatherTimer = 10;
+function seasonColor(summerColor, fallColor, winterColor, springColor) {
+  if (weather == "Summer") {
+    fill(summerColor);
+  } else if (weather == "Fall") {
+    fill(fallColor);
+  } else if (weather == "Winter") {
+    fill(winterColor);
+  } else {
+    fill(springColor);
+  }
 }
